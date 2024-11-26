c77fce5c177e242bda31b9279c2290eb4a4459eb
Vincent <vincenzoyee@gmail.com>
2024-11-26 00:25:35 -0700

    update help drawer

--- components/RewindComponents/Drawers/HelpDrawer/HelpDrawer.tsx
+++ components/RewindComponents/Drawers/HelpDrawer/HelpDrawer.tsx
@@ -24,11 +24,14 @@ const HelpDrawer: React.FC<HelpDrawerProps> = () => {
                         (I usually find that the older the timeframe gets, the more records it may be missing).
                     </p>
                     <p className="text-md">
-                        Your watch history data is only stored on your device unless you explicitly create a shareable rewind.
-                        Only the minimum amount of information required to create your rewind is stored.
+                        YouTube does not provide watch time and only provides the time you click on a video so watch time is 
+                        an approximated attribute. To approximate for watch time, I assume you either watch the whole video or 
+                        the duration until you click on a different youtube video.
+                        (There's some extra stuff I calculate but that's the general idea so it should hopefully be a good enough approximation).
                     </p>
                     <p className="text-md">
-                        Watch time is an approximated attribute as YouTube does not provide this data.
+                        Your watch history data is only stored on your device unless you explicitly create a shareable rewind.
+                        Only the minimum amount of information required to create your rewind is stored.
                     </p>
                 </div>
             </div>
