a3ad89770d69462b0410bb13014dd952105efc2c
Vincent <vincenzoyee@gmail.com>
2025-04-12 22:24:26 -0600

    add apos

--- app/maintenance/page.tsx
+++ app/maintenance/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     return (
         <div className="w-full flex items-center justify-center h-dvh relative">
           <div className="text-page-white flex flex-col gap-4">
-            <h6 className="text-2xl md:text-4xl">We're undergoing maintenance</h6>
+            <h6 className="text-2xl md:text-4xl">We&apos;re undergoing maintenance</h6>
             <p className="text-xl md:text-2xl">See{" "}
               <Link href="https://x.com/yuyu933933" target="blank">
                 @yuyu933933
