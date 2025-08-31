49488b905579fd2694b562aa51e906b20fdca99d
Vincent Yee <vincenzoyee@gmail.com>
2025-08-31 19:31:48 -0400

    Revert "Revert "add NEXT_PUBLIC_POCKETBASE_ADDRESS""

--- lib/pocketbase/pocketbase.ts
+++ lib/pocketbase/pocketbase.ts
@@ -52,11 +52,7 @@ export interface TypedPocketBase extends PocketBase {
     collection(idOrName: 'rewinds'): RecordService<RewindDBEntry>
 }
 
-const POCKETBASE_ADDRESS = (
-    process.env.NODE_ENV === "production" ? 
-    "https://backend.holorewind.com" :
-    "http://127.0.0.1:8090"
-)
+const POCKETBASE_ADDRESS = process.env.NEXT_PUBLIC_POCKETBASE_ADDRESS;
 
 const pb = new PocketBase(POCKETBASE_ADDRESS) as TypedPocketBase;
 
