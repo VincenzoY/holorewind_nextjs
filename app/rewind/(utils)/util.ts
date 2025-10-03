79b8540c1f7e836cf4005c05465c2fa280db8450
Vincent Yee <vincenzoyee@gmail.com>
2025-10-03 10:51:26 -0600

    remove utils

--- app/rewind/(utils)/util.ts
+++
@@ -1,7 +0,0 @@
-import { RewindDBEntry } from "@/lib/pocketbase/pocketbase";
-import { cache } from "react";
-import clientPocketbase from "@/lib/pocketbase/client/pocketbase_client";
-
-export const getRewindDataById = cache(async (id: string): Promise<RewindDBEntry> => {
-    return (await clientPocketbase.fetchRecordFromCollectionById<RewindDBEntry>("rewind", id))
-})
