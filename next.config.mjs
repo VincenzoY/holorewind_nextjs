14bf53f7fecae15b8124064fd105a0044f6d0e4d
Vincent Yee <vincenzoyee@gmail.com>
2025-10-22 00:39:31 -0600

    update next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'yt3.ggpht.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yt3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
